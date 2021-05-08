01c4827fb2627e3503828a499ab61ad887238e53
ahnhyunjin1 <a01020813765@gmail.com>
2021-05-08 22:04:38 +0900

    split.file

---
+++ split.js
@@ -0,0 +1,14 @@
+//string.split("",[divide])
+
+// const myUrl = 'https://example.com/?id=123345",name=Lion&age=28'
+// myUrl.split('&');
+
+// myUrl.split(/&|\?/);
+
+// console.log(myUrl);
+
+const b = 'JavaScript'.split('');
+console.log(b);
+
+const c = 'JavaScript'.split();
+console.log(c);
