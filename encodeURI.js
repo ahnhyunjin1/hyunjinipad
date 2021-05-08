f3bd5edde9e7088fedd9a334bce5f360586c3533
ahnhyunjin1 <a01020813765@gmail.com>
2021-05-08 22:04:38 +0900

    문자 인코딩하기.file

---
+++ encodeURI.js
@@ -0,0 +1,6 @@
+/* encodeURI(string) 
+   encodeURIComponent(string)*/
+
+console.log(encodeURI('http://example.com/ㄱㅇㅇ ㄱㅇㅇ ㅍㅇㅈ.html'));
+
+console.log(encodeURIComponent('http://example.com/ㄱㅇㅇ ㄱㅇㅇ ㅍㅇㅈ.html'));
