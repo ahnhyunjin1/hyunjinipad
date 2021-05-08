b57bbaece7878ccd543bf8cf0594cc5eec9f0eed
ahnhyunjin1 <a01020813765@gmail.com>
2021-05-08 22:04:38 +0900

    문자열with식.file

---
+++ someofstring.js
@@ -0,0 +1,12 @@
+//string+string+...
+//`${string1}${string2}${string}`
+
+const userName = 'cat';
+
+const tag = 
+'<div calss="container">' + 'HI' + userName + 'Nim' + '</div>';
+
+console.log(tag);
+
+const str2 = `123 x 123 = ${123 * 123} .`;
+console.log(str2);
