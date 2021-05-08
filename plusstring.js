7a180e9a9a880f973651de39858f9079691d3854
ahnhyunjin1 <a01020813765@gmail.com>
2021-05-08 22:04:38 +0900

    문자열합치기.file

---
+++ plusstring.js
@@ -0,0 +1,9 @@
+//string+string+string
+//`${string}${string2}${string3}`
+
+const country = 'korea';
+const states = 'seoul';
+
+console.log(country + states);
+
+console.log(`${country}${states}`);
