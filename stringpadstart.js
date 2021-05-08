3087c230acb564e2b846814ee32a8f25c05192a9
ahnhyunjin1 <a01020813765@gmail.com>
2021-05-08 22:04:38 +0900

    문자열 길이맞추기.file

---
+++ stringpadstart.js
@@ -0,0 +1,10 @@
+//syntax
+//string.padStart(total,addstring)
+//string.padEnd(tota.addstring)
+// console.log('5'.padStart(2,'0')); // start
+
+// console.log('ff'.padEnd(6,'0')); //end
+
+// console.log('123'.padStart(3,'0')); //same string no change
+
+console.log('ff'.padStart(6));
