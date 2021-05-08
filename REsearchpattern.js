e28177dd9a454f6732ec605fb939ca4c407f537f
ahnhyunjin1 <a01020813765@gmail.com>
2021-05-08 22:04:38 +0900

    정규표현식특정패턴검색.file

---
+++ REsearchpattern.js
@@ -0,0 +1,7 @@
+//search collect string
+//syntax = /pattern/.test(string) => true/ false
+console.log(/J/.test('Javascript'));
+console.log(/^iP/.test('iPhone'));
+console.log(/\d/.test('recipe'));
+console.log(/Java.*/.test('Javascript'));
+console.log(/c.*p/.test('code recipe'));
