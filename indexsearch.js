0da66c646560b2b91e86412fc588d77bc44a07f2
ahnhyunjin1 <a01020813765@gmail.com>
2021-05-08 15:35:59 +0900

    문자열searh.file

---
+++ indexsearch.js
@@ -0,0 +1,19 @@
+const mystring = 'javascript learn';
+
+const a1 = mystring.indexOf('javascript');
+console.log(a1);
+
+const a2 = mystring.indexOf('learn');
+console.log(a2);
+
+const a3 = mystring.lastIndexOf('a');
+console.log(a3);
+
+const b1 = mystring.indexOf('HTML');
+console.log(b1);
+
+const b2 = mystring.indexOf('J');
+console.log(b2);
+
+const c1 = mystring.indexOf('learn',12);
+console.log(c1);
