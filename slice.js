17cd0bbe47806c48aba3c467232c6c199c4b138f
ahnhyunjin1 <a01020813765@gmail.com>
2021-05-08 15:35:59 +0900

    슬라이스,섭스트링.file

---
+++ slice.js
@@ -0,0 +1,12 @@
+const mystring = 'javascript'
+// console.log(mystring.slice(0,4));
+// console.log(mystring.slice(0));
+// console.log(mystring.substring(0,4));
+// console.log(mystring.substring(0));
+
+console.log(mystring.slice(8,-1)); //p
+console.log(mystring.slice(-10,-1)); //javascript
+
+//substring = -1 ->0 change
+console.log(mystring.substring(3,-3)) //== (3,0)
+
