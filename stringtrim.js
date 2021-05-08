914816c6868173a7569e707f2e23baa15d026317
ahnhyunjin1 <a01020813765@gmail.com>
2021-05-08 15:35:59 +0900

    trim.file

---
+++ stringtrim.js
@@ -0,0 +1,15 @@
+//syntax
+//string.trim => trimming
+
+// const targetString1 = '     hello       ';
+// console.log(targetString1);
+// const trimmedString1 = targetString1.trim();
+// console.log(trimmedString1);
+
+// const targetString2 = ' eat\n apple\n';
+// const trimmedString2 = targetString2.trim();
+// console.log(trimmedString2);
+
+const targetString3 = '        apple. grape.        ';
+const trimmedString3 = targetString3.trim();
+console.log(trimmedString3);
